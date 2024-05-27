0360af337b2e560d951fc0fe56403cdb4024cb53
NiehBright <169137767+NiehBright@users.noreply.github.com>
2024-05-27 23:59:04 +0700

    Map
    Ngay cái bản là nơi nhân vật sẽ bắt đầu

---
+++ Assets/Assets/Scripts/Player/State/PlayerJumpState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpState : PlayerState
+{
+    public PlayerJumpState(Player player, PlayerStateMachine stateMachine, string animBoolName) : base(player, stateMachine, animBoolName)
+    {
+    }
+}
